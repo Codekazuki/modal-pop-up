26ed10af143250aa82722c29eb07d63c503128ca
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-14 18:22:17 +0100

    modal pop up mark up and javascript added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn");
+const close = document.querySelector(".close");
+
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
